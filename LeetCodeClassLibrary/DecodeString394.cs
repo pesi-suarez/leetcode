d4bfedd1acd9a0a6b8588408fdac8e36d29cb396
Pesi <pedro.suarez.miranda@gmail.com>
2018-07-16 02:01:26 +0100

    Empiezo el problema 394.
    Renombro la clase del programa 707 y la meto en el namespace.
    
    Añado proyecto de tests unitarios.

---
+++ LeetCodeClassLibrary/DecodeString394.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeClassLibrary
+{
+    public class DecodeString394
+    {
+        /*
+        public string DecodeString(string str)
+        {
+
+        }
+        */
+
+        public List<string> Tokenize(string str)
+        {
+            List<string> result = new List<string>();
+            int pos = 0;
+            string workingStr = str;
+            while (pos != workingStr.Length-1)
+            {
+                result.Add(ExtractToken(workingStr, ref pos));
+                if (pos != workingStr.Length-1)
+                    workingStr = workingStr.Substring(pos+1);
+            }
+            return result;
+        }
+
+        public string ExtractToken(string str, ref int pos)
+        {
+            int headPos = 0;
+            string head = GetHead(str, ref headPos);
+            if (headPos == str.Length)
+            {
+                pos = str.Length;
+                return head;
+            }
+            else
+            {
+                string tail = string.Empty;
+                int bracketCounter = 1;
+                for (int i = headPos+1; i < str.Length; i++)
+                {
+                    tail += str[i];
+
+                    if (str[i] == '[') bracketCounter++;
+                    else if (str[i] == ']') bracketCounter--;
+
+                    if (bracketCounter == 0)
+                    {
+                        pos = i;
+                        return head + tail;
+                    }
+                }
+            }
+
+            return string.Empty; //Will never get here.
+        }
+
+        public string GetHead(string str, ref int pos)
+        {
+            int i;
+            string result = string.Empty;
+            for (i = 0; i < str.Length; i++)
+            {
+                if (str[i] != '[') result += str[i];
+                else break;
+            }
+            if (i < str.Length) result += '[';
+            pos = i;
+            return result;
+        }
+    }
+}
---
+++ LeetCodeUnitTesting/DecodeString394UnitTesting.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeClassLibrary;
+using System.Collections.Generic;
+
+namespace LeetCodeUnitTesting
+{
+    [TestClass]
+    public class DecodeString394UnitTesting
+    {
+        [TestMethod]
+        public void GetHead()
+        {
+            DecodeString394 dec = new DecodeString394();
+            string str1 = "asdfdshj";
+            int pos = 0;
+            Assert.AreEqual(str1, dec.GetHead(str1, ref pos));
+            Assert.AreEqual(str1.Length, pos);
+
+            string str2 = "amcis2[k]a5[hg3[ft]]";
+            Assert.AreEqual("amcis2[", dec.GetHead(str2, ref pos));
+            Assert.AreEqual(6, pos);
+        }
+
+        [TestMethod]
+        public void ExtractToken()
+        {
+            DecodeString394 dec = new DecodeString394();
+            string str1 = "asdfdshj";
+            int pos = 0;
+            Assert.AreEqual(str1, dec.ExtractToken(str1, ref pos));
+            Assert.AreEqual(str1.Length, pos);
+
+            string str2 = "amcis2[k]a5[hg3[ft]]";
+            Assert.AreEqual("amcis2[k]", dec.ExtractToken(str2, ref pos));
+            Assert.AreEqual(8, pos);
+
+        }
+
+        [TestMethod] //Vemos que la asignación de strings es una copia.
+        public void RefStrings()
+        {
+            string a = "aaa";
+            string b = a;
+            a = "bbb";
+            Assert.AreNotEqual(a, b);
+        }
+
+        [TestMethod]
+        public void Tokenize()
+        {
+            DecodeString394 dec = new DecodeString394();
+            string str2 = "amcis2[k]a5[hg3[ft]]";
+            List<string> tokens = dec.Tokenize(str2);
+            Assert.AreEqual("amcis2[k]", tokens[0]);
+            Assert.AreEqual("a5[hg3[ft]]", tokens[1]);
+        }
+    }
+}
