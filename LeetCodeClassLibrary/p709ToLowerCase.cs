58d6c48e538cd411974a73ae25d961612f9cb0c6
Pesi <pedro.suarez.miranda@gmail.com>
2019-05-03 19:50:44 +0100

    Añado solución para el problema 709 - To Lower Case

---
+++ LeetCodeClassLibrary/p709ToLowerCase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p709ToLowerCase
+{
+    public class Solution
+    {
+        //Esto es correcto y LeetCode lo acepta. De hecho es la solución más rápida (aunque puede dar más tiempo)
+        /*
+        public string ToLowerCase(string str)
+        {
+            return str.ToLower();
+        }
+        */
+
+        //Esto es prácticamente equivalente
+        public string ToLowerCase(string str)
+        {
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(str);
+            byte[] resultBytes = new byte[asciiBytes.Length];
+            for (int i = 0; i < asciiBytes.Length; i++)
+            {
+                if (asciiBytes[i] >= 65 && asciiBytes[i] <= 90)
+                    resultBytes[i] = (byte)(asciiBytes[i] + 32);
+                else
+                    resultBytes[i] = asciiBytes[i];
+            }
+            return Encoding.Default.GetString(resultBytes);
+        }
+
+    }
+}
---
+++ LeetCodeUnitTesting/utp709ToLowerCase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using p709ToLowerCase;
+
+namespace LeetCodeUnitTesting
+{
+    [TestClass]
+    public class utp709ToLowerCase
+    {
+        [TestMethod]
+        public void DescriptionTests()
+        {
+            Solution solution = new Solution();
+            Assert.AreEqual("hello", solution.ToLowerCase("Hello"));
+            Assert.AreEqual("here", solution.ToLowerCase("here"));
+            Assert.AreEqual("lovely", solution.ToLowerCase("LOVELY"));
+        }
+    }
+}
