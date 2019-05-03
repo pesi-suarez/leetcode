6201b86835da0dee021fe97707867da47e3822e8
Pesi <pedro.suarez.miranda@gmail.com>
2019-05-03 19:06:04 +0100

    Añado solción para el problema 1021 - Remove Otermost Parenthesis

---
+++ LeetCodeClassLibrary/p1021RemoveOutermostParenthesis.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1021RemoveOutermostParenthesis
+{
+    public class Solution
+    {
+        public string RemoveOuterParentheses(string S)
+        {
+            StringBuilder resultBuilder = new StringBuilder();
+            int openingCounter = 0;
+            int startingIndex = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == '(')
+                    openingCounter++;
+                else
+                    openingCounter--;
+                if (openingCounter == 0)
+                {
+                    resultBuilder.Append(S.Substring(startingIndex + 1, i - startingIndex - 1));
+                    startingIndex = i + 1;
+                }
+            }
+            return resultBuilder.ToString();
+        }
+    }
+}
---
+++ LeetCodeUnitTesting/utp1021RemoveOutermostParenthesis.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using p1021RemoveOutermostParenthesis;
+
+namespace LeetCodeUnitTesting
+{
+    [TestClass]
+    public class utp1021RemoveOutermostParenthesis
+    {
+        [TestMethod]
+        public void DescriptionTests()
+        {
+            Solution solution = new Solution();
+            Assert.AreEqual("()()()", solution.RemoveOuterParentheses("(()())(())"));
+            Assert.AreEqual("()()()()(())", solution.RemoveOuterParentheses("(()())(())(()(()))"));
+            Assert.AreEqual(string.Empty, solution.RemoveOuterParentheses("()"));
+        }
+    }
+}
