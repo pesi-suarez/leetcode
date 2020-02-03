4af432d09e52ce47c564ac5f208aa14b67aac284
Pesi <pedro.suarez.miranda@gmail.com>
2020-02-03 19:27:39 +0000

    Añado solución para el problema 2, "Add Two Numbers"

---
+++ LeetCodeClassLibrary/p2AddTwoNumbers.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+
+namespace p2AddTwoNumbers
+{
+    //NOTA: No incluir la clase ListNode en el código que se envía.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode() { }
+        public ListNode(int x) { val = x; }
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            ListNode iterator = this;
+            while (iterator != null)
+            {
+                builder.Append(iterator.val.ToString());
+                iterator = iterator.next;
+            }
+            return builder.ToString();
+        }
+    }
+
+    /* NOTA: 
+     * Tardé unos 30 minutos en resolver el problema con la solución más simple (lineal).
+     * Me equivoqué una vez porque no tuve en cuenta el carry final
+     * Me dice que es más rápido que el 89,67% de los envíos y ocupa menos espacio que el 9,09% de los envíos.
+     */
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode result = new ListNode((l1.val + l2.val) % 10);
+            int carry = (l1.val + l2.val) / 10;
+
+            ListNode l1Pointer = l1.next;
+            ListNode l2Pointer = l2.next;
+            ListNode resultPointer = result;
+
+            while(l1Pointer != null && l2Pointer != null)
+            {
+                ListNode nextDigit = new ListNode((l1Pointer.val + l2Pointer.val + carry) % 10);
+                carry = (l1Pointer.val + l2Pointer.val + carry) / 10;
+                resultPointer.next = nextDigit;
+
+                l1Pointer = l1Pointer.next;
+                l2Pointer = l2Pointer.next;
+                resultPointer = nextDigit;
+            }
+
+            while (l1Pointer != null)
+            {
+                ListNode nextDigit = new ListNode((l1Pointer.val + carry) % 10);
+                carry = (l1Pointer.val + carry) / 10;
+                resultPointer.next = nextDigit;
+
+                l1Pointer = l1Pointer.next;
+                resultPointer = nextDigit;
+            }
+
+            while (l2Pointer != null)
+            {
+                ListNode nextDigit = new ListNode((l2Pointer.val + carry) % 10);
+                carry = (l2Pointer.val + carry) / 10;
+                resultPointer.next = nextDigit;
+
+                l2Pointer = l2Pointer.next;
+                resultPointer = nextDigit;
+            }
+
+            if (carry != 0)
+            {
+                ListNode nextDigit = new ListNode(carry);
+                resultPointer.next = nextDigit;
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ LeetCodeUnitTesting/utp2AddTwoNumbers.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using p2AddTwoNumbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeUnitTesting
+{
+    [TestClass]
+    public class utp2AddTwoNumbers
+    {
+        [TestMethod]
+        public void DescriptionTest()
+        {
+            //342 + 465 = 807
+            ListNode a = new ListNode
+            {
+                val = 2,
+                next = new ListNode
+                {
+                    val = 4,
+                    next = new ListNode
+                    {
+                        val = 3,
+                        next = null
+                    }
+                }
+            };
+
+            ListNode b = new ListNode
+            {
+                val = 5,
+                next = new ListNode
+                {
+                    val = 6,
+                    next = new ListNode
+                    {
+                        val = 4,
+                        next = null
+                    }
+                }
+            };
+
+            Solution solution = new Solution();
+            Assert.AreEqual("708", solution.AddTwoNumbers(a, b).ToString());
+        }
+
+        [TestMethod]
+        public void FailedTest()
+        {
+            ListNode a = new ListNode
+            {
+                val = 5,
+                next = null
+            };
+
+            ListNode b = new ListNode
+            {
+                val = 5,
+                next = null
+            };
+
+            Solution solution = new Solution();
+            Assert.AreEqual("01", solution.AddTwoNumbers(a, b).ToString());
+        }
+    }
+}
