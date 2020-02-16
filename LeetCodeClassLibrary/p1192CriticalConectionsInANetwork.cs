7181bba568b5d38b0d5bb689b368688cefd41ff1
Pesi <pedro.suarez.miranda@gmail.com>
2020-02-16 01:03:54 +0000

    Añado solución cuadrática (NxM) para el problema 1192, Critical Connections in a Network.
    Falla en el testcase 9 por un timeout :(

---
+++ LeetCodeClassLibrary/p1192CriticalConectionsInANetwork.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+/*
+ * NOTA: Para la solución cuadrática (MxN) me dio un timeout. Pero entiendo que como solución era correcta. Seguro tiene que haber una solución de menor complejidad en algún curso de teoría de grafos.
+ * En efecto, 8 de 12 casos de test pasados.
+ */
+
+namespace p1192CriticalConectionsInANetwork
+{
+    public class Solution
+    {
+        private class GraphNode
+        {
+            public int Value { get; set; }
+            public List<GraphNode> Neighbours { get; set; }
+        }
+        public IList<IList<int>> CriticalConnections(int n, IList<IList<int>> connections)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            GraphNode graph = BuildGraph(connections);
+            foreach (var edge in connections)
+            {
+                List<int> visited = new List<int>();
+                if (!TraversableWithout(graph, edge, visited, n))
+                    result.Add(edge.ToList());
+            }
+            return result;
+        }
+
+        private GraphNode BuildGraph(IList<IList<int>> connections)
+        {
+            Dictionary<int, GraphNode> nodes = new Dictionary<int, GraphNode>();
+            foreach (var edge in connections)
+            {
+                GraphNode node0;
+                GraphNode node1;
+                if (!nodes.ContainsKey(edge[0]))
+                {
+                    node0 = new GraphNode { Value = edge[0], Neighbours = new List<GraphNode>() };
+                    nodes.Add(edge[0], node0);
+                }
+                else
+                    node0 = nodes[edge[0]];
+
+                if (!nodes.ContainsKey(edge[1]))
+                {
+                    node1 = new GraphNode { Value = edge[1], Neighbours = new List<GraphNode>() };
+                    nodes.Add(edge[1], node1);
+                }
+                else
+                    node1 = nodes[edge[1]];
+                nodes[edge[0]].Neighbours.Add(node1);
+                nodes[edge[1]].Neighbours.Add(node0);
+            }
+            return nodes[connections[0][0]];
+        }
+
+        //TODO: No sé si una lista es lo más correcto.
+        private bool TraversableWithout(GraphNode graph, IList<int> edge, List<int> visited, int n)
+        {
+            visited.Add(graph.Value);
+            if (visited.Count == n)
+                return true;
+            bool result = false;
+            foreach (GraphNode neighbour in graph.Neighbours)
+            {
+                if (!visited.Contains(neighbour.Value) && !EdgeBetween(graph, neighbour, edge))
+                {
+                    result = result || TraversableWithout(neighbour, edge, visited, n);
+                }
+                    
+            }
+            return result;
+        }
+
+        private bool EdgeBetween(GraphNode graph, GraphNode neighbour, IList<int> edge)
+        {
+            if (graph.Value == edge[0] && neighbour.Value == edge[1])
+                return true;
+            else if (graph.Value == edge[1] && neighbour.Value == edge[0])
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ LeetCodeUnitTesting/utp1192CriticalConectionsInANetwork.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using p1192CriticalConectionsInANetwork;
+using System.Collections.Generic;
+
+namespace LeetCodeUnitTesting
+{
+    [TestClass]
+    public class utp1192CriticalConectionsInANetwork
+    {
+        [TestMethod]
+        public void DescriptionTest()
+        {
+            IList<IList<int>> conections = new List<IList<int>>
+            {
+                new List<int> {0, 1},
+                new List<int> {1, 2},
+                new List<int> {2, 0},
+                new List<int> {1, 3}
+            };
+            Solution solution = new Solution();
+            var sol = solution.CriticalConnections(4, conections);
+        }
+
+    }
+}
