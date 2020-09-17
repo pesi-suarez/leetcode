797d330efd7b6b8a2daa318d00abe477ff13a953
Pesi <pedro.suarez.miranda@gmail.com>
2020-09-18 00:49:40 +0100

    Añado solución aceptada al problema 937

---
+++ LeetCodeClassLibrary/p0937ReorderDataInLogFiles.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace p0937ReorderDataInLogFiles
+{
+    public class Solution
+    {
+        //Accepted answer (ft 43.02%)
+        public string[] ReorderLogFiles(string[] logs)
+        {
+            List<string> letterLogs = logs.Where(log => log.Split(' ')[1][0] > '9' || log.Split(' ')[1][0] < '0').ToList();
+            var digitLogs = logs.Where(log => log.Split(' ')[1][0] <= '9' && log.Split(' ')[1][0] >= '0');
+
+            letterLogs.Sort((a, b) => {
+                var aContent = string.Join(' ', a.Split(' ').Skip(1));
+                var bContent = string.Join(' ', b.Split(' ').Skip(1));
+                if (string.Compare(aContent, bContent) != 0)
+                    return string.Compare(aContent, bContent);
+                else
+                {
+                    var aIdentifier = a.Split(' ')[0];
+                    var bIdentifier = b.Split(' ')[0];
+                    return string.Compare(aIdentifier, bIdentifier);
+                }
+
+            });
+            return letterLogs.Concat(digitLogs).ToArray();
+        }
+    }
+}
---
+++ LeetCodeUnitTesting/utp0937ReorderDataInLogFiles.cs
@@ -0,0 +1,22 @@
+﻿using p0937ReorderDataInLogFiles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTesting
+{
+    public class utp0937ReorderDataInLogFiles
+    {
+        [Fact]
+        public void DescriptionTests()
+        {
+            Solution solution = new Solution();
+            string[] logs = { "dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero" };
+            string[] expected = { "let1 art can", "let3 art zero", "let2 own kit dig", "dig1 8 1 5 1", "dig2 3 6" };
+            string[] result = solution.ReorderLogFiles(logs);
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
