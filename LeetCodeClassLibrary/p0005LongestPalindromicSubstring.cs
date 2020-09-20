a055f7ab9922e8beac415e09a5209b570748964c
Pesi <pedro.suarez.miranda@gmail.com>
2020-09-21 00:14:54 +0100

    Añado propuesta de solución al problema 0005. No funciona de momento

---
+++ LeetCodeClassLibrary/p0005LongestPalindromicSubstring.cs
@@ -0,0 +1,54 @@
+﻿namespace p0005LongestPalindromicSubstring
+{
+    public class Solution
+    {
+        public string LongestPalindrome(string s)
+        {
+            string longestPalindrome = string.Empty;
+            for (int i = 0; i < s.Length; i++)
+            {
+                string evenPalindrome = GenerateEvenPalindrome(s, i);
+                string oddPalindrome = GenerateOddPalindrome(s, i);
+                string candidatePalindrome = evenPalindrome.Length > oddPalindrome.Length ? evenPalindrome : oddPalindrome;
+                longestPalindrome = candidatePalindrome.Length > longestPalindrome.Length ? candidatePalindrome : longestPalindrome;
+            }
+
+            return longestPalindrome;
+        }
+
+        private string GenerateEvenPalindrome(string s, int i)
+        {
+            if (i == s.Length - 1)
+                return string.Empty;
+
+            string palindrome = string.Empty;
+            int limit = i < s.Length / 2 ? i + 1 : s.Length-i-1;
+            //center of odd-length string
+            if (i == s.Length - i - 1)
+                limit--;
+            for (int j = 0; j < limit; j++)
+            {
+                if (s[i - j] == s[i + j + 1])
+                    palindrome = s[i - j] + palindrome + s[i + j + 1];
+                else break;
+            }
+            return palindrome;
+        }
+
+        private string GenerateOddPalindrome(string s, int i)
+        {
+            if (i == s.Length - 1)
+                return s[i].ToString();
+
+            string palindrome = s[i].ToString();
+            int limit = i < s.Length / 2 ? i : s.Length-i-1;
+            for (int j = 0; j < limit; j++)
+            {
+                if (s[i - j - 1] == s[i + j + 1])
+                    palindrome = s[i - j - 1] + palindrome + s[i + j + 1];
+                else break;
+            }
+            return palindrome;
+        }
+    }
+}
---
+++ LeetCodeUnitTesting/utp0005LongestPalindromicSubstring.cs
@@ -0,0 +1,26 @@
+﻿using p0005LongestPalindromicSubstring;
+using Xunit;
+
+namespace LeetCodeUnitTesting
+{
+    public class utp0005LongestPalindromicSubstring
+    {
+        [Fact]
+        public void DescriptiopnTests()
+        {
+            Solution solution = new Solution();
+            Assert.Equal("bab", solution.LongestPalindrome("babad"));
+            Assert.Equal("bb", solution.LongestPalindrome("cbbd"));
+        }
+
+        [Fact]
+        public void SubmissionTests()
+        {
+            Solution solution = new Solution();
+            Assert.Equal("a", solution.LongestPalindrome("a"));
+            Assert.Equal("ccc", solution.LongestPalindrome("ccc"));
+            Assert.Equal("aaaa", solution.LongestPalindrome("aaaa"));
+            Assert.Equal("bb", solution.LongestPalindrome("abb"));
+        }
+    }
+}
