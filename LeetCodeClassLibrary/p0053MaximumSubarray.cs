9c4e4132798ccac3bd1a4b1711466af85b5e3998
Pesi <pedro.suarez.miranda@gmail.com>
2020-09-27 15:52:52 +0100

    Añado solución aceptada (cuadrática) para el problema 53 "Maximum Subarray"

---
+++ LeetCodeClassLibrary/p0053MaximumSubarray.cs
@@ -0,0 +1,20 @@
+﻿namespace p0053MaximumSubarray.cs
+{
+    public class Solution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int candidate = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < nums.Length; j++)
+                {
+                    sum += nums[j];
+                    candidate = sum > candidate ? sum : candidate;
+                }
+            }
+            return candidate;
+        }
+    }
+}
---
+++ LeetCodeUnitTesting/utp0053MaximumSubarray.cs
@@ -0,0 +1,19 @@
+﻿using p0053MaximumSubarray.cs;
+using Xunit;
+
+namespace LeetCodeUnitTesting
+{
+    public class utp0053MaximumSubarray
+    {
+        [Fact]
+        public void DescriptionTests()
+        {
+            Solution solution = new Solution();
+            Assert.Equal(6, solution.MaxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4  }));
+            Assert.Equal(1, solution.MaxSubArray(new int[] { 1 }));
+            Assert.Equal(0, solution.MaxSubArray(new int[] { 0 }));
+            Assert.Equal(-1, solution.MaxSubArray(new int[] { -1 }));
+            Assert.Equal(-2147483647, solution.MaxSubArray(new int[] { -2147483647 })); 
+        }
+    }
+}
