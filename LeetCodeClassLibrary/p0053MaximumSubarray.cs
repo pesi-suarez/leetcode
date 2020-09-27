71a9999e26641206ca45b1fc6f7b395cc17843db
Pesi <pedro.suarez.miranda@gmail.com>
2020-09-27 16:22:56 +0100

    Añado solución lineal para el problema 53

--- LeetCodeClassLibrary/p0053MaximumSubarray.cs
+++ LeetCodeClassLibrary/p0053MaximumSubarray.cs
@@ -1,7 +1,35 @@
-﻿namespace p0053MaximumSubarray.cs
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p0053MaximumSubarray.cs
 {
     public class Solution
     {
+        public int MaxSubArray(int[] nums)
+        {
+            if (nums.All(num => num >= 0))
+                return nums.Sum();
+            else if (nums.All(num => num < 0))
+                return nums.Max();
+            else
+            {
+                int currentSum = 0;
+                int maxSum = nums[0];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    currentSum += nums[i];
+                    if (currentSum < 0)
+                        currentSum = 0;
+                    else
+                        maxSum = currentSum > maxSum ? currentSum : maxSum;
+                }
+                return maxSum;
+            }
+        }
+
+        //O(n2)
+        /*
         public int MaxSubArray(int[] nums)
         {
             int candidate = nums[0];
@@ -16,5 +44,6 @@
             }
             return candidate;
         }
+        */
     }
 }
