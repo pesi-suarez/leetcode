f8ae6f405b7ede0e49aafabf0aaedabed1554143
Pesi <pedro.suarez.miranda@gmail.com>
2020-09-28 18:51:44 +0100

    Añado propuesta de solución incorrecta para el problema 0003.
    El paso no recursivo no creo que pueda hacerse con complejidad lineal

--- LeetCodeClassLibrary/p0003LongestSubstringWithoutRepeatingCharacters.cs
+++ LeetCodeClassLibrary/p0003LongestSubstringWithoutRepeatingCharacters.cs
@@ -1,9 +1,73 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace p0003LongestSubstringWithoutRepeatingCharacters
 {
     public class Solution
     {
+        //Creo que esto no funciona porque no hay manera de generar correctamente c y d de forma lineal.
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 0)
+                return 0;
+            if (s.Length == 1)
+                return 1;
+            int a = LengthOfLongestSubstring(s.Substring(0, s.Length / 2));
+            int b = LengthOfLongestSubstring(s.Substring(s.Length / 2));
+            int c = LengthOfLongestCentralSubstring(s);
+            int d = LengthOfLongestCentralSubstring(Reverse(s));
+            List<int> data = new List<int> { a, b, c, d };
+            return data.Max();
+        }
+
+        private int LengthOfLongestCentralSubstring(string s)
+        {
+            int substringLength = 0;
+            bool leftBound = false;
+            bool rightBound = false;
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length/2+1; i++)
+            {
+                if (!leftBound && s.Length / 2 - i - 1 >= 0)
+                {
+                    if (dict.ContainsKey(s[s.Length / 2 - i - 1]))
+                        leftBound = true;
+                    else
+                    {
+                        substringLength++;
+                        dict.Add(s[s.Length / 2 - i - 1], 0);
+                    }
+                        
+                }
+ 
+            }
+            for (int i = 0; i < s.Length / 2 + 1; i++)
+            {
+                if (!rightBound && s.Length / 2 + i < s.Length)
+                {
+                    if (dict.ContainsKey(s[s.Length / 2 + i]))
+                        rightBound = true;
+                    else
+                    {
+                        substringLength++;
+                        dict.Add(s[s.Length / 2 + i], 0);
+                    }
+
+                }
+            }
+            return substringLength;
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        //O(n2) solution
+        /*
         public int LengthOfLongestSubstring(string s)
         {
             int maxSubstringLength = 0;
@@ -24,5 +88,6 @@ namespace p0003LongestSubstringWithoutRepeatingCharacters
             }
             return maxSubstringLength;
         }
+        */
     }
 }
--- LeetCodeUnitTesting/utp0003LongestSubstringWithoutRepeatingCharacters.cs
+++ LeetCodeUnitTesting/utp0003LongestSubstringWithoutRepeatingCharacters.cs
@@ -14,5 +14,29 @@ namespace LeetCodeUnitTesting
             Assert.Equal(3, solution.LengthOfLongestSubstring("pwwkew"));
             Assert.Equal(0, solution.LengthOfLongestSubstring(""));
         }
+
+        [Fact]
+        public void SubmissionTests()
+        {
+            Solution solution = new Solution();
+            Assert.Equal(8, solution.LengthOfLongestSubstring("pmukfzdskwdyne"));
+            Assert.Equal(11, solution.LengthOfLongestSubstring("bziuwnklhqzrxnb"));
+            /*
+             * Se repiten B, Z y N
+             * El substring más largo es iuwnklhqzrx, efectivamente de longitud 11.
+             * B Z I U W N K L H Q Z R X N B
+             * 
+             * Traza:
+             * B Z I U W N [K] L H Q Z R X N B | K
+             * B Z I U W [N] K L H Q Z R X N B | K, N
+             * B Z I U [W] N K L H Q Z R X N B | K, N, W
+             * ...
+             * [B] Z I U W N K L H Q Z R X N B | K, N, W, U, I, Z, B
+             * B Z I U W N K [L] H Q Z R X N B | K, N, W, U, I, Z, B, L
+             * B Z I U W N K L [H] Q Z R X N B | K, N, W, U, I, Z, B, L, H
+             * B Z I U W N K L H [Q] Z R X N B | K, N, W, U, I, Z, B, L, H, Q
+             * B Z I U W N K L H Q [Z] R X N B | K, N, W, U, I, Z, B, L, H, Q Boundright
+             */
+        }
     }
 }
