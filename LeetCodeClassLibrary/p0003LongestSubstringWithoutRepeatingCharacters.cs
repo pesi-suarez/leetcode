a13caf416ffdf5f8439e6f49a493a59ef1b91d77
Pesi <pedro.suarez.miranda@gmail.com>
2020-09-28 16:30:11 +0100

    Añado solución aceptada para el problema 0003 "Longest Substring Without Repeating Characters"

---
+++ LeetCodeClassLibrary/p0003LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace p0003LongestSubstringWithoutRepeatingCharacters
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int maxSubstringLength = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                Dictionary<char, int> dict = new Dictionary<char, int>();
+                int currentSubstringLength = 0;
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (dict.ContainsKey(s[j]))
+                    {
+                        break;
+                    }
+                    dict.Add(s[j], j);
+                    currentSubstringLength++;
+                    maxSubstringLength = currentSubstringLength > maxSubstringLength ? currentSubstringLength : maxSubstringLength;
+                }
+            }
+            return maxSubstringLength;
+        }
+    }
+}
---
+++ LeetCodeUnitTesting/utp0003LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,18 @@
+﻿using p0003LongestSubstringWithoutRepeatingCharacters;
+using Xunit;
+
+namespace LeetCodeUnitTesting
+{
+    public class utp0003LongestSubstringWithoutRepeatingCharacters
+    {
+        [Fact]
+        public void DescriptionTests()
+        {
+            Solution solution = new Solution();
+            Assert.Equal(3, solution.LengthOfLongestSubstring("abcabcbb"));
+            Assert.Equal(1, solution.LengthOfLongestSubstring("bbbbb"));
+            Assert.Equal(3, solution.LengthOfLongestSubstring("pwwkew"));
+            Assert.Equal(0, solution.LengthOfLongestSubstring(""));
+        }
+    }
+}
