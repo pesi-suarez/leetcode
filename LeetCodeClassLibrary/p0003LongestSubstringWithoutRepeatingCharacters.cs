93be2a9ea1d11391aaed83a40f5e1cb4c3633f4b
Pesi <pedro.suarez.miranda@gmail.com>
2020-09-29 17:14:37 +0100

    Modifico la solución aceptada del problema 0003 para obtener complejidad O(n)

--- LeetCodeClassLibrary/p0003LongestSubstringWithoutRepeatingCharacters.cs
+++ LeetCodeClassLibrary/p0003LongestSubstringWithoutRepeatingCharacters.cs
@@ -1,28 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace p0003LongestSubstringWithoutRepeatingCharacters
 {
     public class Solution
     {
+        //O(n) solution, based on https://bit.ly/3jfbRYu
+        //j nunca puede volver hacia atrás, así nunca se considera un substring con una repetición anidada
         public int LengthOfLongestSubstring(string s)
         {
-            int maxSubstringLength = 0;
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            int maxLength = 0;
+            int j = 0;
             for (int i = 0; i < s.Length; i++)
             {
-                Dictionary<char, int> dict = new Dictionary<char, int>();
-                int currentSubstringLength = 0;
-                for (int j = i; j < s.Length; j++)
+                if (dict.ContainsKey(s[i]))
                 {
-                    if (dict.ContainsKey(s[j]))
-                    {
-                        break;
-                    }
-                    dict.Add(s[j], j);
-                    currentSubstringLength++;
-                    maxSubstringLength = currentSubstringLength > maxSubstringLength ? currentSubstringLength : maxSubstringLength;
+                    j = Math.Max(j, dict[s[i]] + 1); 
+                    dict[s[i]] = i;
                 }
+                else
+                    dict.Add(s[i], i);
+                maxLength = Math.Max(maxLength, i - j + 1);
             }
-            return maxSubstringLength;
+            return maxLength;
         }
+
     }
 }
