882dbb945a3052f8070e256315276a5602a9ce7b
Pesi <pedro.suarez.miranda@gmail.com>
2021-07-14 10:45:34 +0100

    Added simple, quadratic, accepted solution for problem 506 "Subarray Sum Equals K"

---
+++ LeetCodeClassLibrary/p0560SubarraySumEqualsK.cs
@@ -0,0 +1,23 @@
+﻿namespace p0560SubarraySumEqualsK
+{
+    public class Solution
+    {
+        public int SubarraySum(int[] nums, int k)
+        {
+            int kCount = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == k)
+                    kCount++;
+                int acum = nums[i];
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    acum += nums[j];
+                    if (acum == k)
+                        kCount++;
+                }
+            }
+            return kCount;
+        }
+    }
+}
